6bdca29e556c71a9f281fdf51862d44eea3671a8
Steven Crouse <sdcrouse91@gmail.com>
2018-08-06 22:36:40 +0000

    Done.

--- looping.rb
+++ looping.rb
@@ -1,9 +1,12 @@
+#require 'pry'
 def looping
 	#your code here
 	loop do
 	  puts "Wingardium Leviosa"
+	  #binding.pry
 	end
 end
 
 #call your method here
 looping
+# Note: as of 8/6/2018, Control C will NOT exit out of this lab's infinite loop.
